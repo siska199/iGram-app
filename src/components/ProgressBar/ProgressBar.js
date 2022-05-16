e0d771d982f6d69f8836bf5260227b1bcb41a4e6
siska199 <siska.apriana.1703226@students.um.ac.id>
2022-05-16 08:48:45 +0700

    update progress bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,24 +1,39 @@
-import React from "react";
+import React, { useEffect } from "react";
 import useStorage from "../../hook/useStorage";
+import { collection, doc, setDoc, serverTimestamp } from "firebase/firestore";
+import { db } from "../../firebase.config";
 
-const ProgressBar = ({file,setFile}) => {
-  const {url,progress,error} = useStorage(file)
-  if(progress==100){
-    setTimeout(() => {
-      setFile(false)
-    }, 1500);
-  }
+const ProgressBar = ({ file, setFile }) => {
+  const { url, progress, error } = useStorage(file);
+  const imageCollRef = doc(collection(db, "images"));
 
-    console.log("url: ", url)
-    console.log("progress", progress)
-    console.log("err: ", error)
+  useEffect(() => {
+    console.log("ooo");
+    if (progress == 100) {
+      setTimeout(() => {
+        setFile(false);
+        handleUploadFirestore();
+      }, 1500);
+    }
+  }, [url]);
+
+  const handleUploadFirestore = async () => {
+    setDoc(imageCollRef, {
+      filename: file.name,
+      url,
+      createdAt: Date.now(),
+    }).then((res) => {
+      console.log("ress addDoc:", res);
+    });
+  };
+
   return (
     <>
-      <progress id="file" value={progress} max="100">  
+      <progress id="file" value={progress} max="100">
         {progress}%
       </progress>
     </>
   );
 };
 
-export default ProgressBar
+export default ProgressBar;
